524f18f3ad76b48ad59623d24713759459694876
Vladimir Kukharenko <kukharenko.va@phystech.edu>
2023-05-14 01:14:06 +0200

    base version of licensed app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,95 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import "@/styles/globals.css";
+import "@/styles/classes.css";
+
+import { useState } from "react";
+import axios from "axios";
+import {
+  SismoConnectButton,
+  SismoConnectClientConfig,
+  SismoConnectResponse,
+  AuthType,
+} from "@sismo-core/sismo-connect-react";
+
+export const sismoConnectConfig: SismoConnectClientConfig = {
+  // You can create a new Sismo Connect app at https://factory.sismo.io
+  appId: "0x3c48bcee0d62fe08d9236f186f245415",
+};
+
+type UserType = {
+  id: string;
+};
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [verifiedUser, setVerifiedUser] = useState<UserType>(null);
+
+  async function verify(response: SismoConnectResponse) {
+    // First we update the react state to show the loading state
+    setLoading(true);
+
+    try {
+      // We send the response to our backend to verify the proof
+      const res = await axios.post(`/api/level-1-verify-user`, {
+        response,
+      });
+
+      const user = res.data;
+
+      // If the proof is valid, we update the user react state to show the user profile
+      setVerifiedUser({
+        id: user.id,
+      });
+    } catch (e) {
+      // Else if the proof is invalid, we show an error message
+      setError("Invalid response");
+      console.error(e);
+    } finally {
+      // We set the loading state to false to show the user profile
+      setLoading(false);
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <div className="container">
+      {!verifiedUser && (
+          <>
+            <h1>ZK License verification</h1>
+            <p className="subtitle-page">
+                Your ownership of the license will be verified without revealing your address and public private keys. 
+            </p>
+            <SismoConnectButton
+            config={sismoConnectConfig}
+            auths={[{ authType: AuthType.VAULT }]}
+            claims={[{ groupId: "0x3adc181791f9fb91199bb67f3d70baa2" }]}
+            signature={{
+              message: "License verification",
+              isSelectableByUser: false, // Not allow the user to change the message (here his user name) during the Sismo Connect flow
+            }}
+            onResponse={(response: SismoConnectResponse) => verify(response)}
+            loading={loading}
+            text="Register with Sismo"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <>{error}</>
+          </>
+      )}
+      {verifiedUser && (
+          <>
+            <h1>You have verified your license</h1>
+            <p className="subtitle-page">
+              Application was automatically started. You can close browser.
+            </p>
+            <div className="profile-container">
+              <h2 style={{ marginBottom: 10 }}>User Profile</h2>
+              <div style={{ marginBottom: 10 }}>
+                <b>UserId:</b>
+                <p>{verifiedUser.id}</p>
+              </div>
+            </div>
+          </>
+        )}
+    </div>
   )
 }
---
+++ src/pages/api/level-1-verify-user.ts
@@ -0,0 +1,80 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  SismoConnect,
+  SismoConnectServerConfig,
+  AuthType,
+  SismoConnectVerifiedResult,
+} from "@sismo-core/sismo-connect-server";
+import { exec } from 'child_process';
+
+/************************************************ */
+/************* CONFIGURE SISMO CONNECT ********** */
+/************************************************ */
+
+// define the SismoConnect configuration
+const sismoConnectConfig: SismoConnectServerConfig = {
+  // you can create a new Sismo Connect app at https://factory.sismo.io
+  appId: "0x3c48bcee0d62fe08d9236f186f245415",
+};
+
+// create a SismoConnect instance
+const sismoConnect = SismoConnect(sismoConnectConfig);
+
+/************************************************ */
+/***************** THE API ROUTE **************** */
+/************************************************ */
+
+// this is the API route that is called by the SismoConnectButton
+export default async function handler(req: NextApiRequest, res: NextApiResponse<UserType | void>) {
+  const { response } = req.body;
+
+  console.log("response", response);
+  try {
+    const result: SismoConnectVerifiedResult = await sismoConnect.verify(response, {
+      auths: [{ authType: AuthType.VAULT }],
+      claims: [{ groupId: "0x3adc181791f9fb91199bb67f3d70baa2" }],
+      signature: {
+        message: "License verification",
+        isSelectableByUser: false,
+      },
+    });
+
+    const user = {
+      // the userId is an app-specific, anonymous identifier of a vault
+      // userId = hash(userVaultSecret, appId).
+      id: result.getUserId(AuthType.VAULT),
+      name: result.getSignedMessage(),
+    };
+
+    const command = 'echo "Hello, World!"';
+
+    // Start protected software
+    const childProcess = exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Execution error: ${error.message}`);
+        return;
+      }
+
+      if (stderr) {
+        console.error(`Output error: ${stderr}`);
+        return;
+      }
+
+      console.log(`Result: ${stdout}`);
+    });
+
+    // Обработка события завершения выполнения команды
+    childProcess.on('exit', (code, signal) => {
+      console.log(`Executed with code ${code} and signal ${signal}`);
+    });
+
+
+    res.status(200).send(user);
+
+    // process.exit(0);
+  } catch (e: any) {
+    console.error(e);
+    res.status(400).send(null);
+  }
+}
